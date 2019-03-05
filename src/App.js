25ad4cf71d76b8ea42372174871c198dbf1c1d64
Nadya <nadeia.ak@gmail.com>
2019-03-05 14:03:43 +0300

    Nadya-Rasha

--- src/App.js
+++ src/App.js
@@ -86,6 +86,28 @@ class App extends Component {
                     </div>
                   </div>
                 </div>
+                <div className="col-lg-4 col-md-6 col-12">
+                  <div className="card">
+                    <div className="image">
+                      <img
+                        className="card-img-top img-fluid"
+                        src={authors[3].imageUrl}
+                        alt="J.R.R Tolkein"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        <span>
+                          {authors[3].first_name}
+                          {authors[3].last_name}
+                        </span>
+                      </h5>
+                      <small className="card-text">
+                        {authors[3].books.length} books
+                      </small>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
